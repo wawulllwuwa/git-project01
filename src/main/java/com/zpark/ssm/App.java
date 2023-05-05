8eba1f168baf90f88d15a360656b7d89f8cf7d9a
zy <zy@163.com>
2023-05-05 18:18:59 +0800

    second

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git,second");
+        System.out.println("hello,git,three");
     }
 }
