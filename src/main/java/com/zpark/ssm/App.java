28cbdcc5a6e30b4fcd5bf669485b659b4158b30a
zy <zy@163.com>
2023-05-05 18:17:01 +0800

    first

---
+++ src/main/java/com/zpark/ssm/App.java
@@ -0,0 +1,8 @@
+package com.zpark.ssm;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git,second");
+    }
+}
